44ba0fc49d89f727f6ab24f4145d3d029d0a7dc4
jmyager <jmyager6@gmail.com>
2018-05-31 13:40:06 -0400

    directory setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('CheeseBurger');
+INSERT INTO burgers (burger_name) VALUES ('BaconBurger');
+INSERT INTO burgers (burger_name) VALUES ('GuacBurger');
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(table, cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(table, val, cb) {
+        orm.insertOne("burgers", val, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(table, val, id, cb) {
+        orm.updateOne("burgers", val, id, function(res) {
+            cb(res);
+        })
+    }
+};
+
+module.exports(burger);
